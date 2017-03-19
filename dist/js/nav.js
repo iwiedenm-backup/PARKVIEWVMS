393c656e85182ea88686b1d203d556ab3fe32dad
iwiedenm <iwiedenm@lion.lmu.edu>
2017-03-19 12:00:46 -0700

    Add files

---
+++ dist/js/nav.js
@@ -0,0 +1,18 @@
+document.getElementById("navMenu").innerHTML =
+'<ul class="nav navbar-nav">'+
+  '<li class="activse"><a href="index.html">Home <span class="sr-only">(current)</span></a></li>'+
+
+  '<li class="dropdown">'+
+    '<a href="#" class="dropdown-toggle" data-toggle="dropdown">About Us <span class="caret"></span></a>'+
+    '<ul class="dropdown-menu" role="menu">'+
+      '<li><a href="o_overview.html">Overview & Services</a></li>'+
+      '<li><a href="o_mission.html">Mission</a></li>'+
+      '<li><a href="o_gallery.html">Gallery</a></li>'+
+    '</ul>'+
+  '</li>'+
+  '<li><a href="o_staff.html">Staff</a></li>'+
+  '<li><a href="o_events.html">News & Events</a></li>'+
+  '<li><a href="o_clientportal.html">Client Portal</a></li>'+
+  '<li><a href="o_staffportal.html">Staff Portal</a></li>'+
+'</ul>'
+;
---
+++ dist/js/navProfile.js
@@ -0,0 +1,18 @@
+document.getElementById("navMenu").innerHTML =
+'<ul class="nav navbar-nav">'+
+  '<li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>'+
+
+  '<li class="dropdown">'+
+    '<a href="#" class="dropdown-toggle" data-toggle="dropdown">About Us <span class="caret"></span></a>'+
+    '<ul class="dropdown-menu" role="menu">'+
+      '<li><a href="#">Overview & Services</a></li>'+
+      '<li><a href="#">Mission</a></li>'+
+      '<li><a href="#">Gallery</a></li>'+
+    '</ul>'+
+  '</li>'+
+  '<li><a href="index.html">Staff</a></li>'+
+  '<li><a href="index.html">News & Events</a></li>'+
+  '<li><a href="index.html">Client Portal</a></li>'+
+  '<li><a href="index.html">Staff Portal</a></li>'+
+'</ul>'
+;
